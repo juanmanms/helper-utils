dee71388ac9c651bd85d14d032ac6813c59ffa1d
juanmanms <juanmanms@gmail.com>
2026-09-23 14:57:25 +0200

    Agrega la propiedad title al componente Button para mejorar la accesibilidad

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,13 +4,15 @@ interface ButtonProps {
   onClick: () => void;
   children: React.ReactNode;
   className?: string;
+  title?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children, className }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, children, className, title }) => {
   return (
     <button
       onClick={onClick}
       className={`px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+      title={title}
     >
       {children}
     </button>
